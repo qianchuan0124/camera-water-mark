35f9b1fdf671389680efcb18186c237c86a80dba
qianchuan <1119502724@qq.com>
2025-06-05 01:53:17 +0800

    没有权限时增加相关提示

--- app/view/main_window.py
+++ app/view/main_window.py
@@ -1,13 +1,12 @@
+import sys
+from pathlib import Path
 from qfluentwidgets import (FluentWindow)
-from qfluentwidgets import setTheme, Theme, NavigationItemPosition
+from qfluentwidgets import setTheme, Theme, NavigationItemPosition, MessageBox
 from qfluentwidgets import FluentIcon as FIF
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
-from app.view.home_interface import HomeInterface
-from app.view.setting_interface import SettingInterface
+from PyQt5.QtCore import QCoreApplication
 from app.config import ASSETS_PATH, ROOT_PATH
-from app.utils.logger import setup_logger
-logger = setup_logger("main")
 
 LOGO_PATH = f"{ASSETS_PATH}/logo.png"
 
@@ -17,12 +16,35 @@ class MainWindow(FluentWindow):
         setTheme(Theme.DARK)
 
         super().__init__()
+
         self.initWindow()
 
+        self.check_permissions(ROOT_PATH)
+
+        from app.view.home_interface import HomeInterface
+        from app.view.setting_interface import SettingInterface
         self.homeInterface = HomeInterface(self)
         self.settingInterface = SettingInterface(self)
 
         self.initNavigation()
+
+    def check_permissions(self, path: str):
+    # 检查是否有读写权限
+        try:
+            test_path = Path(path) / "test_permission"
+            test_path.mkdir(parents=True, exist_ok=True)
+            test_path.rmdir()  # 删除测试文件夹
+        except PermissionError:
+            w = MessageBox(
+                QCoreApplication.translate("MainWindow", "没有读写权限"), 
+                QCoreApplication.translate("MainWindow", "请检查文件夹权限或以管理员身份运行程序。"), 
+                self.window()
+            )
+            w.yesButton.setText(QCoreApplication.translate("MainWindow", "确认"))
+            w.cancelButton.setHidden(True)
+            if w.exec():
+                sys.exit()
+        
 
     def initWindow(self):
         self.resize(1050, 600)
@@ -35,7 +57,6 @@ class MainWindow(FluentWindow):
         w, h = desktop.width(), desktop.height()
         self.move(w // 2 - self.width() // 2, h // 2 - self.height() // 2)
 
-        logger.info(f"root path: {ROOT_PATH}")
         self.show()
 
     def initNavigation(self):
@@ -47,5 +68,9 @@ class MainWindow(FluentWindow):
             self.tr("设置"),
             NavigationItemPosition.BOTTOM,
         )
+
+        from app.utils.logger import setup_logger
+        logger = setup_logger("main")
+        logger.info(f"root path: {ROOT_PATH}")
 
         self.switchTo(self.homeInterface)
